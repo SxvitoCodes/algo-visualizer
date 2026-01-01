7955eac6d94b75d19cf2362b34ac0f1875c60d0c
cotne <cotne.sxvitaridze@gmail.com>
2026-01-01 17:02:07 +0400

    feat: add navbar + css cleanup

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <div className="p-6 flex justify-center items-center">
+        <h1 className="text-white text-lg font-bold">Algo Visualizer</h1>
+        <button className="ml-4 text-white font-bold py-2 px-4 rounded">Visualize!</button>
+    </div>
+  )
+}
+
+export default Navbar
